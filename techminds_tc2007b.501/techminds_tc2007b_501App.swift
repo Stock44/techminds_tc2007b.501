2e78625b30798dc19920ac15d487cd75360c6f8f
Hiram Muñoz <hirammunozr@outlook.com>
2023-05-05 15:39:12 -0600

    initial commit

---
+++ techminds_tc2007b.501/techminds_tc2007b_501App.swift
@@ -0,0 +1,17 @@
+//
+//  techminds_tc2007b_501App.swift
+//  techminds_tc2007b.501
+//
+//  Created by Alumno on 05/05/23.
+//
+
+import SwiftUI
+
+@main
+struct techminds_tc2007b_501App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
