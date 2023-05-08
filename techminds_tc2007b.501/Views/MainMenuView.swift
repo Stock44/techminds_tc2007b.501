ff1c2cfc4b394dbf0f8ad4cc17ce2d1e59e2f77a
Hiram Muñoz <hirammunozr@outlook.com>
2023-05-08 11:30:38 -0600

    Added views and main menu view

---
+++ techminds_tc2007b.501/Views/MainMenuView.swift
@@ -0,0 +1,20 @@
+//
+//  MainMenuView.swift
+//  techminds_tc2007b.501
+//
+//  Created by Alumno on 08/05/23.
+//
+
+import SwiftUI
+
+struct MainMenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainMenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainMenuView()
+    }
+}
