df4528dfb9fb33ffcf294a58a5432a5986f9aa6a
Hiram Muñoz <hirammunozr@outlook.com>
2023-05-08 13:46:20 -0600

    navigation stack

--- techminds_tc2007b.501/Views/MainMenuView.swift
+++ techminds_tc2007b.501/Views/MainMenuView.swift
@@ -11,12 +11,12 @@ struct MainMenuView: View {
     var body: some View {
         HStack {
             VStack{
-                Image("menucard")
+                Image(systemName:"menucard").resizable().aspectRatio(contentMode: .fit).foregroundColor(Color("accent2 darker")).padding(.all)
                 Text("Tarjetas")
             }.background(Color("accent2 lighter"))
                 .cornerRadius(16)
-                
             VStack {
+                Image(systemName: "rectangle.3.group").resizable().aspectRatio(contentMode: .fit).foregroundColor(Color("secondary darker"))
                 Text("Colecciones")
             }.background(Color("secondary lighter"))
                 .cornerRadius(16)
