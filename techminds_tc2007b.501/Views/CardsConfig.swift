c67f5b208c7c36dba1e435f04a47461e2e9a3ebe
Raul <a01197655@tec.mx>
2023-05-26 13:50:12 -0600

    b

---
+++ techminds_tc2007b.501/Views/CardsConfig.swift
@@ -0,0 +1,27 @@
+//
+//  CardsConfig.swift
+//  techminds_tc2007b.501
+//
+//  Created by Alumno on 26/05/23.
+//
+
+import SwiftUI
+
+struct CardsConfig: View {
+    var body: some View {
+        ZStack {
+            VStack {
+                
+            }
+            VStack {
+                
+            }
+        }
+    }
+}
+
+struct CardsConfig_Previews: PreviewProvider {
+    static var previews: some View {
+        CardsConfig()
+    }
+}
